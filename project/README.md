68a831c43c0524962cbc2bda687c9f668d059a42
Artem Melnikov <melnikovam10@gmail.com>
2021-09-04 22:58:29 +0300

    Auto-generated commit [ci skip]

--- project/README.md
+++
@@ -1,2 +0,0 @@
-# thishedgehogdoesnotexist
-Simple js site to showcase my friend's algorithm for generating hedgehogs
--- project/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  imgs: "https://github.com/Melnikovartem/thishedgehogdoesnotexist/tree/master/src/assets/imgs"
-};
